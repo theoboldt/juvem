9090c046f664b9ae7f639875327c1d1aff980a64
theoboldt <e.s.t.m52@gmx.de>
2015-12-21 23:49:14 +0100

    Added form for modal actions⁄

---
+++ src/AppBundle/Form/ModalActionType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ModalActionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('area', HiddenType::class)
+            ->add('action', HiddenType::class)
+            ->add('id', HiddenType::class)
+            ->add('cancel', ButtonType::class, array('attr' => array('class' => 'btn-default'), 'label' => 'Abbrechen'))
+            ->add('submit', SubmitType::class, array('attr' => array('class' => 'btn-primary', 'label' => 'Speichern')))
+        ;
+    }
+}
