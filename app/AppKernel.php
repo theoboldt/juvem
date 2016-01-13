30cd3c82151de8ab0b7a374ffcb2dc0344819f86
theoboldt <e.s.t.m52@gmx.de>
2016-01-13 23:12:59 +0100

    Added FOSUserBundle to requirements⁄

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new AppBundle\AppBundle(),
         );
 
