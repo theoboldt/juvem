9986d524b74c4f8068f17081a0b296f3c89c6ec2
theoboldt <e.s.t.m52@gmx.de>
2016-01-14 23:53:22 +0100

    Added user management controller⁄

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+
+use AppBundle\Twig\Extension\BootstrapGlyph;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/user/list", name="user_list")
+     */
+    public function listAction(Request $request)
+    {
+        return $this->render('user/list.html.twig');
+    }
+
+    /**
+     * Data provider for event list grid
+     *
+     * @Route("/user/list.json", name="user_list_data")
+     */
+    public function listDataAction(Request $request)
+    {
+        $glyph = new BootstrapGlyph();
+
+
+        $userManager = $this->container->get('fos_user.user_manager');
+        $entityList = $userManager->findUsers();
+
+        $userList = array();
+
+        /** @var User $entity */
+        foreach ($entityList as $entity) {
+            $entityRoles    = $entity->getRoles();
+
+            $roles  = '';
+            if (in_array('ROLE_USER', $entityRoles)) {
+                $roles .= sprintf(
+                    '<span title="%s">%s</span>',
+                    'Benutzer',
+                    $glyph->bootstrapGlyph('pawn')
+                );
+            }
+            if (in_array('ROLE_SUPER_ADMIN', $entityRoles)) {
+                $roles .= sprintf(
+                    '<span title="%s">%s</span>',
+                    'Administrator',
+                    $glyph->bootstrapGlyph('king')
+                );
+            }
+
+
+            $userList[] = array(
+                'uid'       => $entity->getUid(),
+                'email'     => $entity->getEmail(),
+                'nameFirst' => $entity->getNameFirst(),
+                'nameLast'  => $entity->getNameLast(),
+                'roles'     => $roles
+            );
+        }
+
+        return new JsonResponse($userList);
+    }
+}
