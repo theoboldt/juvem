7f4a7d99fecb73443d540f73b5a36575490b6bc1
theoboldt <e.s.t.m52@gmx.de>
2016-01-21 14:23:34 +0100

    Added missing dependency⁄

---
+++ src/AppBundle/Twig/Extension/ModalDialog.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+class ModalDialog extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter(
+                'modalDialog',
+                array($this, 'modalDialog'),
+                array('pre_escape' => 'html', 'is_safe' => array('html'), 'needs_environment' => true)
+            ),
+        );
+
+    }
+
+    /**
+     * Create html for a bootstrap glyph
+     *
+     * @param \Twig_Environment $twig
+     * @return string Html bootstrap glyph snippet
+     * @internal param string $glyph Glyph name
+     */
+    public function modalDialog(\Twig_Environment $twig, $title)
+    {
+        $data   = array('modalId' => 'xxx',
+            'modalTitle' => $title,
+            'modalYes' => 1,
+            'modalNo' => 2);
+
+        return $twig->render('modal/dialog.html.twig', $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'modal_dialog';
+    }
+}
