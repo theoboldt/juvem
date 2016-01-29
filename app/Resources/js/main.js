073421d69c39f401aaff6efebc518c6614467e37
theoboldt <e.s.t.m52@gmx.de>
2016-01-30 00:21:13 +0100

    Embedded form prototype generation js is now global Added style to embedded form

--- app/Resources/js/main.js
+++ app/Resources/js/main.js
@@ -1,13 +1,28 @@
 jQuery(document).ready(function () {
-    $('.contains-prototype').each(function (index) {
+
+    /**
+     * Handle via prototype injected forms
+     */
+    $('.prototype-container').each(function (index) {
         var element = $(this),
             prototype;
         if (element.attr('data-prototype')) {
             prototype = element.data('prototype');
-            //debugger
+            element.data('index', element.find(':input').length);
+
+            element.find('.prototype-add').on('click', function (e) {
+                e.preventDefault();
+                var index = element.data('index');
+                var newForm = prototype.replace(/__name__/g, index);
+                element.data('index', index + 1);
+
+                element.find('.prototype-elements').append(newForm);
+            });
+            element.find('.prototype-remove').on('click', function (e) {
+                e.preventDefault();
+                $(this).parent().parent().empty();
+            });
         }
-
-
     });
 
 });
