78ffeace354293961a74edab5b00d8716a8786b7
theoboldt <e.s.t.m52@gmx.de>
2016-02-12 22:29:39 +0100

    Corrected bootrstrap js ⁄dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,12 +39,12 @@ module.exports = function (grunt) {
                     'node_modules/bootstrap-table/src/locale/bootstrap-table-de-DE.js',
                     'node_modules/bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
                     'node_modules/bootstrap-sass/assets/javascripts/bootstrap/alert.js',
-                    'node_modules/bootstrap-sass/assets/javascripts/bootstrap/button.js',
+//                    'node_modules/bootstrap-sass/assets/javascripts/bootstrap/button.js',
                     'node_modules/bootstrap-sass/assets/javascripts/bootstrap/tooltip.js',
                     'node_modules/bootstrap-sass/assets/javascripts/bootstrap/popover.js',
                     'node_modules/bootstrap-sass/assets/javascripts/bootstrap/transition.js',
-/*
                     'node_modules/bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
+/*
                     'node_modules/bootstrap-sass/assets/javascripts/bootstrap/scrollspy.js',
                     'node_modules/bootstrap-sass/assets/javascripts/bootstrap/affix.js',
 */
