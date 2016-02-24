9f0af1c422568092f8cec4aa015647662abc3195
theoboldt <e.s.t.m52@gmx.de>
2016-02-24 21:11:38 +0100

    Now using html and txt templated email for participation

--- src/AppBundle/Manager/ParticipationManager.php
+++ src/AppBundle/Manager/ParticipationManager.php
@@ -5,8 +5,10 @@ namespace AppBundle\Manager;
 use AppBundle\Entity\Event;
 use \AppBundle\Entity\Participation as ParticipationEntity;
 use AppBundle\Entity\Participant;
+use AppBundle\Twig\MailGenerator;
 use Swift_Mailer;
 use Symfony\Component\Templating\EngineInterface;
+use Twig_Environment;
 
 class ParticipationManager
 {
@@ -17,56 +19,31 @@ class ParticipationManager
     protected $mailer;
 
     /**
-     * @var EngineInterface
+     * @var MailGenerator
      */
-    protected $templating;
+    protected $mailGenerator;
 
 
-    public function __construct(Swift_Mailer $mailer, EngineInterface $templating)
+    public function __construct(Swift_Mailer $mailer, MailGenerator $mailGenerator)
     {
-        $this->mailer     = $mailer;
-        $this->templating = $templating;
+        $this->mailer        = $mailer;
+        $this->mailGenerator = $mailGenerator;
     }
 
     public function mailParticipationRequested(ParticipationEntity $participation, Event $event)
     {
-        $message = \Swift_Message::newInstance()
-                                 ->setSubject($event->getTitle().' Anmeldung eingegangen')
-                                 ->setFrom('jungschar.vaihingen@gmail.com')
-                                 ->setTo($participation->getEmail())
-                                 ->setBody(
-                                     $this->templating->render(
-                                         'mail/participation.txt.twig',
-                                         array(
-                                             'event'         => $event,
-                                             'participation' => $participation,
-                                             'participants'  => $participation->getParticipants()
-                                         )
-                                     ),
-                                     'text/plain'
-                                 /*
-                                 $this->renderView(
-                                     'mail/participation.html.twig',
-                                     array(
-                                         'salution' => $participation->getSalution(),
-                                         'nameLast' => $participation->getNameLast()
-                                     )
-                                 ),
-                                 'text/html'
-                                 )->addPart(
-                                     $this->renderView(
-                                         'mail/participation.txt.twig',
-                                         array(
-                                             'event' => $event,
-                                             'participation' => $participation,
-                                             'participants'  => $participation->getParticipants()
-                                         )
-                                     ),
-                                     'text/plain'
-                                 */
-                                 );
-        $this->mailer->send($message);
 
+        $message = $this->mailGenerator->getMessage(
+            'participation', array(
+                            'event'         => $event,
+                            'participation' => $participation,
+                            'participants'  => $participation->getParticipants()
+                        )
+        );
+
+        $message->setTo($participation->getEmail());
+
+        $this->mailer->send($message);
     }
 
     /**
---
+++ src/AppBundle/Twig/MailGenerator.php
@@ -0,0 +1,49 @@
+<?php
+namespace AppBundle\Twig;
+
+use Swift_Message;
+use Twig_Environment;
+
+class MailGenerator
+{
+    protected $twig;
+
+    public function __construct(Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * Create a swift message by using txt and html template
+     *
+     * @param  string $path Full path to twig template
+     * @param  array  $parameters Parameters for template
+     * @return Swift_Message      The email
+     */
+    public function getMessageByPath($path, $parameters = array())
+    {
+        $template = $this->twig->loadTemplate($path); // Define your own schema
+
+        $subject  = $template->renderBlock('subject', $parameters);
+        $bodyHtml = $template->renderBlock('body_html', $parameters);
+        $bodyText = $template->renderBlock('body_text', $parameters);
+
+        return Swift_Message::newInstance()
+                            ->setFrom('jungschar.vaihingen@gmail.com')
+                            ->setSubject($subject)
+                            ->setBody($bodyText, 'text/plain')
+                            ->addPart($bodyHtml, 'text/html');
+    }
+
+    /**
+     * Create a swift message by using identifier
+     *
+     * @param  string $identifier Twig template identifier
+     * @param  array  $parameters Parameters for template
+     * @return Swift_Message      The email
+     */
+    public function getMessage($identifier, $parameters = array())
+    {
+        return $this->getMessageByPath('mail/' . $identifier . '.email.twig', $parameters);
+    }
+}
