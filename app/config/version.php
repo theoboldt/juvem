e70855f718e512953a9f7dbd389b11bf10b28bf0
theoboldt <e.s.t.m52@gmx.de>
2016-03-08 11:39:23 +0100

    Adding file integrity check/revision information to resources

---
+++ app/config/version.php
@@ -0,0 +1,22 @@
+<?php
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/** @var ContainerBuilder $container */
+$container->setParameter('app.version', 'v1');
+
+
+$fileRevision = function($path, $raw = false) use($container) {
+	$rootDir = $container->getParameter('kernel.root_dir');
+	$pathFull = $rootDir.'/../web/'.$path;
+
+	if (is_file($pathFull) && is_readable($pathFull)) {
+		return hash_file('sha256', $pathFull, $raw);
+	} else {
+		return 0;
+	}
+};
+
+$container->setParameter('app.version.hash.js', $fileRevision('js/all.min.js'));
+$container->setParameter('app.version.hash.css', $fileRevision('css/css.js'));
+
+$container->setParameter('app.version.integrity.js', base64_encode($fileRevision('js/all.min.js', true)));
