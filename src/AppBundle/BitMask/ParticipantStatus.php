9d74d1e04d9144bc0ac108cf9c9bbf0e241a7f3d
theoboldt <e.s.t.m52@gmx.de>
2016-10-05 00:20:19 +0200

    Added possibility for users to request withdraw for their own participations

--- src/AppBundle/BitMask/ParticipantStatus.php
+++ src/AppBundle/BitMask/ParticipantStatus.php
@@ -4,13 +4,15 @@ namespace AppBundle\BitMask;
 
 class ParticipantStatus extends BitMaskAbstract
 {
-    const TYPE_STATUS_CONFIRMED = 1;
-    const TYPE_STATUS_PAID      = 2;
-    const TYPE_STATUS_WITHDRAWN = 4;
+    const TYPE_STATUS_CONFIRMED          = 1;
+    const TYPE_STATUS_PAID               = 2;
+    const TYPE_STATUS_WITHDRAWN          = 4;
+    const TYPE_STATUS_WITHDRAW_REQUESTED = 8;
 
-    const LABEL_STATUS_CONFIRMED   = 'bestätigt';
-    const LABEL_STATUS_PAID        = 'bezahlt';
-    const LABEL_STATUS_WITHDRAWN   = 'zurückgezogen';
+    const LABEL_STATUS_CONFIRMED          = 'bestätigt';
+    const LABEL_STATUS_PAID               = 'bezahlt';
+    const LABEL_STATUS_WITHDRAWN          = 'zurückgezogen';
+    const LABEL_STATUS_WITHDRAW_REQUESTED = 'Rücknahme angefragt';
 
     const LABEL_STATUS_UNCONFIRMED = 'unbestätigt';
 }
