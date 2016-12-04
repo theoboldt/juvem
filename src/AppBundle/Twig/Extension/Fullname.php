75ffd7d86aaf7c43a8bf28f7c3b0d2b016fd6774
theoboldt <e.s.t.m52@gmx.de>
2016-12-04 20:50:27 +0100

    Displaying assigned user if any in participation detail

---
+++ src/AppBundle/Twig/Extension/Fullname.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+use AppBundle\Entity\HumanTrait;
+use AppBundle\Entity\User;
+
+/**
+ * Twig extension for fast creation of glyphs
+ *
+ * Class Fullname
+ *
+ * @package AppBundle\Twig\Extension
+ */
+class Fullname extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter(
+                'fullname',
+                array($this,
+                      'fullname'
+                )
+            ),
+        );
+
+    }
+
+    /**
+     * Get template for full name
+     *
+     * @param string|HumanTrait $nameLastOrUser Instance of @see HumanTrait or last name as string
+     * @param string|null       $nameFirst      First name as string or null if Instance of @see HumanTrait was
+     *                                          transmitted as first argument
+     * @return string Html bootstrap glyph snippet
+     */
+    public function fullname($nameLastOrUser, $nameFirst = null)
+    {
+
+        if (method_exists($nameLastOrUser, 'getNameLast') && method_exists($nameLastOrUser, 'getNameFirst')) {
+            return User::fullname($nameLastOrUser->getNameLast(), $nameLastOrUser->getNameFirst());
+        } else {
+            return User::fullname($nameLastOrUser, $nameFirst);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'fullname';
+    }
+}
