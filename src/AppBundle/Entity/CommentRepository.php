59d57b2784db31427ceb1377f2eb2f685a0ccdb1
erik <erik@teqneers.de>
2017-01-23 20:19:49 +0100

    Added comment service, added possibility to fetch comments

--- src/AppBundle/Entity/CommentRepository.php
+++ src/AppBundle/Entity/CommentRepository.php
@@ -46,6 +46,41 @@ class CommentRepository extends EntityRepository
 	}
 
 	/**
+	 * Fetch list of all @see CommentBase for all Participations of transmitted participation
+	 *
+	 * @param Participation $participation Related participation
+	 * @param bool          $returnFlat    Set to true to return flat list of @see ParticipantComment entities, set to
+	 *                                     false to get a list of comments identified by the participant
+	 * @return ParticipantComment[]|array
+	 */
+	public function findForParticipantsOfParticipation(Participation $participation, $returnFlat = false)
+	{
+		$qb = $this->createCommentQueryBuilder('AppBundle:ParticipantComment');
+		$qb->innerJoin('c.participant', 'r')
+		   ->andWhere(
+			   'c.participant IN (SELECT x.aid FROM AppBundle:Participant x WHERE x.participation = :participation)'
+		   );
+		$qb->setParameter('participation', $participation);
+
+		$flatResult = $qb->getQuery()->execute();
+		if ($returnFlat) {
+			return $flatResult;
+		} else {
+			$result = [];
+			/** @var ParticipantComment $comment */
+			foreach ($flatResult as $comment) {
+				$aid = $comment->getParticipant()->getAid();
+				if (isset($result[$aid])) {
+					$result[$aid] = [];
+				}
+				$result[$aid][] = $comment;
+			}
+	        return $result;
+        }
+	}
+
+
+	/**
 	 * Creates comment query builder, adds audit fields, applies order
 	 *
 	 * @param string $alias
@@ -54,10 +89,10 @@ class CommentRepository extends EntityRepository
 	protected function createCommentQueryBuilder($alias)
 	{
 		$qb = $this->getEntityManager()->createQueryBuilder()
-		           ->select('c', 'r')#, 'uc', 'um')
+		           ->select('c', 'r', 'uc', 'um')
 		           ->from($alias, 'c')
-#		           ->leftJoin('c.createdBy', 'uc')
-#		           ->leftJoin('c.modifiedBy', 'um')
+		           ->leftJoin('c.createdBy', 'uc')
+		           ->leftJoin('c.modifiedBy', 'um')
 		           ->orderBy('c.createdAt', 'ASC');
 		return $qb;
 	}
---
+++ src/AppBundle/Manager/CommentManager.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Manager;
+
+use AppBundle\Entity\CommentRepository;
+use AppBundle\Entity\Participant;
+use AppBundle\Entity\ParticipantComment;
+use AppBundle\Entity\Participation;
+use AppBundle\Entity\ParticipationComment;
+use AppBundle\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+
+class CommentManager
+{
+	/**
+	 * Database abstraction
+	 *
+	 * @var Registry
+	 */
+	protected $doctrine;
+
+	/**
+	 * Comment repository
+	 *
+	 * @var CommentRepository
+	 */
+	protected $repository;
+
+	/**
+	 * @var array
+	 */
+	protected $cache;
+
+	/**
+	 * The user currently logged in
+	 *
+	 * @var User|null
+	 */
+	protected $currentUser = null;
+
+	/**
+	 * CommentManager constructor.
+	 *
+	 * @param Registry     $doctrine
+	 * @param TokenStorage $tokenStorage
+	 */
+	public function __construct(Registry $doctrine, TokenStorage $tokenStorage = null)
+	{
+		$this->doctrine = $doctrine;
+		if ($tokenStorage) {
+			$this->user = $tokenStorage->getToken()->getUser();
+		}
+
+		$this->repository = $this->doctrine->getRepository('AppBundle:ParticipantComment');
+	}
+
+	/**
+	 * Fetch list of all @see CommentBase for transmitted @see Participation
+	 *
+	 * @param Participation $participation Related participation
+	 * @return ParticipationComment[]
+	 */
+	public function forParticipation(Participation $participation)
+	{
+		$pid = $participation->getPid();
+		if (!$this->cache[$pid] || !$this->cache[$pid]['comments']) {
+			$this->cache[$pid]['comments'] = $this->repository->findForParticipation($participation);
+		}
+		return $this->cache[$pid]['comments'];
+	}
+
+	/**
+	 * Fetch list of all @see CommentBase for transmitted @see Participant
+	 *
+	 * @param Participant $participant Related participant
+	 * @return ParticipantComment[]
+	 */
+	public function forParticipant(Participant $participant)
+	{
+		$participation = $participant->getParticipation();
+		$pid           = $participation->getPid();
+		$aid           = $participant->getAid();
+
+		if (!$this->cache[$pid] || !$this->cache[$pid]['participants']) {
+			$this->cache[$pid]['participants'] = $this->repository->findForParticipantsOfParticipation($participation);
+		}
+		if ($this->cache[$pid]['participants'][$aid]) {
+			return $this->cache[$pid]['participants'][$aid];
+		} else {
+			return [];
+		}
+	}
+}
