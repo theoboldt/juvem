7f52df9c5f2a4d7c5d59df0881ceff4abe7b1d52
theoboldt <erik@theoboldt.eu>
2017-02-17 21:05:08 +0100

    Vegan is now no longer contained in export⁄

--- src/AppBundle/Export/Sheet/ParticipantsSheet.php
+++ src/AppBundle/Export/Sheet/ParticipantsSheet.php
@@ -17,23 +17,8 @@ use AppBundle\Entity\Event;
 use AppBundle\Entity\Participant;
 use AppBundle\Entity\Participation;
 
-class ParticipantsSheet extends AbstractSheet
+class ParticipantsSheet extends ParticipantsSheetBase
 {
-
-    /**
-     * The event this participant export belongs to
-     *
-     * @var Event
-     */
-    protected $event;
-
-    /**
-     * Stores a list of Participant entities
-     *
-     * @var array
-     */
-    protected $participants;
-
 
     public function __construct(\PHPExcel_Worksheet $sheet, Event $event, array $participants)
     {
@@ -66,15 +51,6 @@ class ParticipantsSheet extends AbstractSheet
         $column->setConverter(
             function ($value, Participant $entity) {
                 return substr($entity->getGender(true), 0, 1);
-            }
-        );
-        $this->addColumn($column);
-
-        $column = EntitySheetColumn::createYesNoColumn('food_vegan', 'Vegan', 'food');
-        $column->setConverter(
-            function ($value, Participant $entity) {
-                $mask = $entity->getFood(true);
-                return $mask->has(ParticipantFood::TYPE_FOOD_VEGAN) ? 'vn' : 'nein';
             }
         );
         $this->addColumn($column);
@@ -158,51 +134,6 @@ class ParticipantsSheet extends AbstractSheet
         $column->setWidth(4);
         $this->addColumn($column);
 
-    }
-
-    public function setHeader($title = null, $subtitle = null)
-    {
-        parent::setHeader($this->event->getTitle(), 'Teilnehmer');
-        $this->row = $this->row - 1; //reset row index by 1
-        parent::setColumnHeaders();
-
-        $this->sheet->getRowDimension($this->row(null, false) - 1)->setRowHeight(-1);
-    }
-
-    public function setBody()
-    {
-
-        /** @var Participant $participant */
-        foreach ($this->participants as $participant) {
-            $row = $this->row();
-
-            /** @var EntitySheetColumn $column */
-            foreach ($this->columnList as $column) {
-                $columnIndex = $column->getColumnIndex();
-                $cellStyle   = $this->sheet->getStyleByColumnAndRow($columnIndex, $row);
-
-                $column->process($this->sheet, $row, $participant);
-
-                $columnDataConditional = $column->getDataCellConditionals();
-                if (count($columnDataConditional)) {
-                    $cellStyle->setConditionalStyles($columnDataConditional);
-                }
-                $cellStyle->getAlignment()->setVertical(
-                    \PHPExcel_Style_Alignment::VERTICAL_TOP
-                );
-                $cellStyle->getBorders()->getBottom()->setBorderStyle(\PHPExcel_Style_Border::BORDER_THIN);
-
-                $columnStyles = $column->getDataStyleCallbacks();
-                if (count($columnStyles)) {
-                    foreach ($columnStyles as $columnStyle) {
-                        if (!is_callable($columnStyle)) {
-                            throw new \InvalidArgumentException('Defined column style callback is not callable');
-                        }
-                        $columnStyle($cellStyle);
-                    }
-                }
-            }
-        }
     }
 
 }
---
+++ src/AppBundle/Export/Sheet/ParticipantsSheetBase.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of the Juvem package.
+ *
+ * (c) Erik Theoboldt <erik@theoboldt.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Export\Sheet;
+
+
+use AppBundle\Entity\Event;
+use AppBundle\Entity\Participant;
+
+abstract class ParticipantsSheetBase extends AbstractSheet
+{
+
+    /**
+     * The event this participant export belongs to
+     *
+     * @var Event
+     */
+    protected $event;
+
+    /**
+     * Stores a list of Participant entities
+     *
+     * @var array
+     */
+    protected $participants;
+
+
+    public function setHeader($title = null, $subtitle = null)
+    {
+        parent::setHeader($this->event->getTitle(), 'Teilnehmer');
+        $this->row = $this->row - 1; //reset row index by 1
+        parent::setColumnHeaders();
+
+        $this->sheet->getRowDimension($this->row(null, false) - 1)->setRowHeight(-1);
+    }
+
+    public function setBody()
+    {
+
+        /** @var Participant $participant */
+        foreach ($this->participants as $participant) {
+            $row = $this->row();
+
+            /** @var EntitySheetColumn $column */
+            foreach ($this->columnList as $column) {
+                $columnIndex = $column->getColumnIndex();
+                $cellStyle   = $this->sheet->getStyleByColumnAndRow($columnIndex, $row);
+
+                $column->process($this->sheet, $row, $participant);
+
+                $columnDataConditional = $column->getDataCellConditionals();
+                if (count($columnDataConditional)) {
+                    $cellStyle->setConditionalStyles($columnDataConditional);
+                }
+                $cellStyle->getAlignment()->setVertical(
+                    \PHPExcel_Style_Alignment::VERTICAL_TOP
+                );
+                $cellStyle->getBorders()->getBottom()->setBorderStyle(\PHPExcel_Style_Border::BORDER_THIN);
+
+                $columnStyles = $column->getDataStyleCallbacks();
+                if (count($columnStyles)) {
+                    foreach ($columnStyles as $columnStyle) {
+                        if (!is_callable($columnStyle)) {
+                            throw new \InvalidArgumentException('Defined column style callback is not callable');
+                        }
+                        $columnStyle($cellStyle);
+                    }
+                }
+            }
+        }
+    }
+
+}
