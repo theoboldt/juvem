7d5aa60ad6831bca3857b30ea7d0be5936679e1b
theoboldt <erik@theoboldt.eu>
2017-03-25 00:17:34 +0100

    Added redirect for trailing slash urls

---
+++ src/AppBundle/Controller/RedirectController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the Juvem package.
+ *
+ * (c) Erik Theoboldt <erik@theoboldt.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class RedirectController
+ *
+ * This class provides redirecting from any url using a trailing slash to the correct controller
+ *
+ * @package AppBundle\Controller
+ */
+class RedirectController extends Controller
+{
+
+    /**
+     * @Route("/{url}", name="remove_trailing_slash",
+     *     requirements={"url" = ".*\/$"}, methods={"GET"})
+     */
+    public function removeTrailingSlashAction(Request $request)
+    {
+        $pathInfo   = $request->getPathInfo();
+        $requestUri = $request->getRequestUri();
+
+        $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
+
+        return $this->redirect($url, 301);
+    }
+}
