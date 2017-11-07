637a9fcddc62b76e334f384e5b6e214ef524c999
theoboldt <erik@theoboldt.eu>
2017-11-07 22:05:21 +0100

    Restored required Assert import

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -14,6 +14,7 @@ use AppBundle\Entity\Audit\CreatedModifiedTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
