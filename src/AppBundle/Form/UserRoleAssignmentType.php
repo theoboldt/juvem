be94868d90b07e398ebedb9e8c7883b3f324b191
erik <erik@theoboldt.eu>
2018-11-15 16:15:40 +0100

    Added possibility to assign employee role to user

--- src/AppBundle/Form/UserRoleAssignmentType.php
+++ src/AppBundle/Form/UserRoleAssignmentType.php
@@ -21,28 +21,29 @@ class UserRoleAssignmentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
-
+    
         $builder
             ->add('uid', HiddenType::class)
             ->add(
                 'role',
                 ChoiceType::class,
-                array(
+                [
                     'label'    => 'Rollen',
-                    'choices'  => array(
+                    'choices'  => [
                         User::ROLE_ADMIN_LABEL              => User::ROLE_ADMIN,
                         User::ROLE_ADMIN_EVENT_LABEL        => User::ROLE_ADMIN_EVENT,
                         User::ROLE_ADMIN_EVENT_GLOBAL_LABEL => User::ROLE_ADMIN_EVENT_GLOBAL,
                         User::ROLE_ADMIN_USER_LABEL         => User::ROLE_ADMIN_USER,
                         User::ROLE_ADMIN_NEWSLETTER_LABEL   => User::ROLE_ADMIN_NEWSLETTER,
-                    ),
+                        User::ROLE_EMPLOYEE_LABEL           => User::ROLE_EMPLOYEE,
+                    ],
                     'expanded' => true,
                     'multiple' => true,
                     'required' => false
-                )
+                ]
             );
     }
-
+    
     public function getBlockPrefix()
     {
         return 'app_bundle_user_role_assignment';
