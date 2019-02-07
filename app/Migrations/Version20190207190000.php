199b523cd3e4be0d7ea824d09794fd7dab237260
erik <erik@theoboldt.eu>
2019-02-07 20:49:37 +0100

    Removing choice option formula field

---
+++ app/Migrations/Version20190207190000.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Formula at choice option is no longer supported
+ */
+final class Version20190207190000 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('ALTER TABLE acquisition_attribute_choice_option DROP price_formula');
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->addSql(
+            'ALTER TABLE acquisition_attribute_choice_option ADD price_formula VARCHAR(255) DEFAULT NULL'
+        );
+    }
+}
