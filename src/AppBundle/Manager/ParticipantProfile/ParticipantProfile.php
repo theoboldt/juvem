5da4329423dfb76b2a1702f2b7b037847d3938cf
theoboldt <erik@theoboldt.eu>
2019-08-04 13:02:29 +0200

    Now showing age in profile export and present symbol if participant has birthday at event

--- src/AppBundle/Manager/ParticipantProfile/ParticipantProfile.php
+++ src/AppBundle/Manager/ParticipantProfile/ParticipantProfile.php
@@ -635,12 +635,18 @@ class ParticipantProfile
 
             $this->addDatum($section, 'Vorname', $participant->getNameFirst());
             $this->addDatum($section, 'Nachname', $participant->getNameLast());
-            $this->addDatum($section, 'Geschlecht', $participant->getGender(true));
-            $birthday = $participant->getBirthday()->format(Event::DATE_FORMAT_DATE);
+
+            $birthday = sprintf(
+                "%s \u{25cb} %s (~%s) Jahre",
+                $participant->getBirthday()->format(Event::DATE_FORMAT_DATE),
+                $participant->getYearsOfLifeAtEvent(),
+                number_format($participant->getAgeAtEvent(1), 1, ',', "'")
+            );
             if ($participant->hasBirthdayAtEvent()) {
                 $birthday .= " \u{1F381}";
             }
-            $this->addDatum($section, 'Geburtsdatum', $birthday);
+            $this->addDatum($section, 'Geburtsdatum/Alter', $birthday);
+            $this->addDatum($section, 'Geschlecht', $participant->getGender(true));
 
             $linkPath = $this->temporaryBarCodeGenerator->createCode(
                 'url:' . $this->urlGenerator->generate(
