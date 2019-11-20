897a3a6f0f4e7c1660e6a49d03f75378e794e4ee
Erik Theoboldt <erik@theoboldt.eu>
2019-11-20 20:09:29 +0100

    Now able to do pre selection

--- app/Resources/js/area/employee.js
+++ app/Resources/js/area/employee.js
@@ -1,3 +1,28 @@
 $(function () {
+    $('#import-employee-actions .btn-apply-to-all').click(function () {
+        var el = $(this),
+            bid = el.data('bid'),
+            action = el.data('action');
 
-});
+        switch (action) {
+            case 'select':
+                var optionId = el.data('id');
+                $('.acq_field_' + bid + ' select').each(function () {
+                    $(this).find('option[value="' + optionId + '"]').prop('selected', true);
+                });
+                $('.acq_field_' + bid + ' input[type="radio"]').each(function () {
+                    $(this).prop('checked', false);
+                });
+                $('.acq_field_' + bid + ' input[value="' + optionId + '"]').each(function () {
+                    $(this).prop('checked', true);
+                });
+                break;
+            case 'clear':
+                $('.acq_field_' + bid + ' input').each(function () {
+                    $(this).val('');
+                });
+                break;
+        }
+    });
+})
+;
