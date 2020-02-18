82b161f49536c9d39d1a86e16bf3fce1f20ec8f2
Erik Theoboldt <erik@theoboldt.eu>
2020-02-18 20:58:31 +0100

    Now able to integrate change overview button

--- src/AppBundle/Entity/ChangeTracking/EntityChangeRepository.php
+++ src/AppBundle/Entity/ChangeTracking/EntityChangeRepository.php
@@ -17,5 +17,66 @@ use Doctrine\ORM\EntityRepository;
  */
 class EntityChangeRepository extends EntityRepository
 {
-
+    /**
+     * Get amount of stored changes for transmitted entity
+     *
+     * @param SupportsChangeTrackingInterface $entity
+     * @return int
+     */
+    public function countAllByEntity(SupportsChangeTrackingInterface $entity): int
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb->select(['COUNT(c)'])
+           ->andWhere('c.relatedId = :relatedId')
+           ->andWhere('c.relatedClass = :relatedClass');
+        
+        $qb->setParameter('relatedId', $entity->getId())
+           ->setParameter('relatedClass', get_class($entity));
+        
+        return (int)$qb->getQuery()->getSingleScalarResult();
+    }
+    
+    /**
+     * Find all changes for transmitted tracked entity
+     *
+     * @param SupportsChangeTrackingInterface $entity
+     * @return array|EntityChange[]
+     */
+    public function findAllByEntity(SupportsChangeTrackingInterface $entity): array
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb->select(['c', 'u'])
+           ->leftJoin('c.responsibleUser', 'u')
+           ->andWhere('c.relatedId = :relatedId')
+           ->andWhere('c.relatedClass = :relatedClass');
+        
+        $qb->setParameter('relatedId', $entity->getId())
+           ->setParameter('relatedClass', get_class($entity));
+        
+        return $qb->getQuery()->execute();
+    }
+    
+    /**
+     * Prepare class name for use in route
+     *
+     * @param string $className
+     * @return string
+     */
+    public static function convertClassNameForRoute(string $className): string
+    {
+        return str_replace('\\', '.', $className);
+    }
+    
+    /**
+     * Exctract class name from route
+     *
+     * @param string $routeName
+     * @return string
+     */
+    public static function convertRouteToClassName(string $routeName): string
+    {
+        return str_replace('.', '\\', $routeName);
+    }
+    
+    
 }
---
+++ src/AppBundle/Twig/Extension/ChangeTrackingButton.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * This file is part of the Juvem package.
+ *
+ * (c) Erik Theoboldt <erik@theoboldt.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Twig\Extension;
+
+
+use AppBundle\Entity\ChangeTracking\EntityChangeRepository;
+use AppBundle\Entity\ChangeTracking\SupportsChangeTrackingInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class ChangeTrackingButton extends AbstractExtension
+{
+    
+    /**
+     * Router used to create the route to details page
+     *
+     * @var UrlGeneratorInterface
+     */
+    protected $router;
+    
+    /**
+     * @var EntityChangeRepository
+     */
+    private $repository;
+    
+    /**
+     * @var AuthorizationChecker
+     */
+    private $authorizationChecker;
+    
+    /**
+     * ChangeTrackingButton constructor.
+     *
+     * @param EntityChangeRepository $repository
+     * @param AuthorizationChecker $authorizationChecker
+     * @param UrlGeneratorInterface $router
+     */
+    public function __construct(
+        EntityChangeRepository $repository, AuthorizationChecker $authorizationChecker, UrlGeneratorInterface $router
+    )
+    {
+        $this->repository           = $repository;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->router               = $router;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter(
+                'changeTrackingButton',
+                [
+                    $this,
+                    'changeTrackingButton'
+                ],
+                [
+                    'pre_escape' => 'html',
+                    'is_safe'    => ['html']
+                ]
+            ),
+        ];
+        
+    }
+    
+    /**
+     * Generate button for changes
+     *
+     * @param SupportsChangeTrackingInterface $entity
+     * @return string
+     */
+    public function changeTrackingButton(SupportsChangeTrackingInterface $entity): string
+    {
+        if ($this->authorizationChecker->isGranted('READ', $entity)) {
+            $changes = 0;
+        } else {
+            $changes = $this->repository->countAllByEntity($entity);
+        }
+        
+        if ($changes === 0) {
+            return '<span class="btn btn-default disabled" title="Änderungen verfolgen"><span class="glyphicon glyphicon-road"  aria-hidden="true"></span> <span class="hidden-xs">Keine Änderungen</span></span>';
+        } else {
+            $path = $this->router->generate(
+                'admin_change_overview',
+                [
+                    'entityId'  => $entity->getId(),
+                    'className' => EntityChangeRepository::convertClassNameForRoute(get_class($entity))
+                ]
+            );
+            return '<span class="btn btn-default disabled" title="Änderungen verfolgen"><span class="glyphicon glyphicon-road"  aria-hidden="true"></span> <span class="hidden-xs">' .
+                   $changes . '&nbsp;Änderung' . (($changes > 1) ? 'en' : '') . '</span></span>';
+        }
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'change_tracking_button';
+    }
+}
