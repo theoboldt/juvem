d01c03ac04912120d7111c8ba4ab60b82c196c0f
Erik Theoboldt <erik@theoboldt.eu>
2020-04-14 17:28:36 +0200

    Introduced inactive string count field for register form

--- src/AppBundle/Form/UserRegistrationFormType.php
+++ src/AppBundle/Form/UserRegistrationFormType.php
@@ -11,33 +11,67 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\EqualTo;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserRegistrationFormType extends AbstractType
 {
-
-    public function buildForm(FormBuilderInterface $builder, array $options = array())
+    
+    public function buildForm(FormBuilderInterface $builder, array $options = [])
     {
         parent::buildForm($builder, $options);
         $builder
             ->add(
-                'nameFirst', TextType::class, array('label'    => 'Vorname',
-                                                    'required' => false
-                           )
+                'nameFirst',
+                TextType::class,
+                [
+                    'label'    => 'Vorname',
+                    'required' => false
+                ]
             )
-            ->add('nameLast', TextType::class, array('label' => 'Nachname'))
+            ->add(
+                'stringCount',
+                NumberType::class,
+                [
+                    'mapped'      => false,
+                    'required'    => true,
+                    'label'       => 'Saiten-Zahl',
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'Bitte zählen Sie Saiten im Bild und geben Sie deren Anzahl in das Eingabefeld ein.'
+                            
+                            ]
+                        
+                        ),
+                        new EqualTo(
+                            [
+                                'value'   => 6,
+                                'message' => 'Bitte zählen Sie die sechs Saiten erneut und geben Sie deren Anzahl nummerisch in das Eingabefeld ein.'
+                            ]
+                        )
+                    ]
+                ]
+            )
+            ->add(
+                'nameLast',
+                TextType::class,
+                ['label' => 'Nachname']
+            )
             ->remove('username');  // we use email as the username
     }
-
+    
     public function getParent()
     {
         return 'FOS\UserBundle\Form\Type\RegistrationFormType';
     }
-
+    
     public function getBlockPrefix()
     {
         return 'app_bundle_user_registration';
     }
-
+    
 }
