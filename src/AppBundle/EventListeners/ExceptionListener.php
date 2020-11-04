c7ba53beeae9341c46d6515e486584c25f18758c
Erik Theoboldt <erik@theoboldt.eu>
2020-11-04 06:03:01 +0100

    Now using ExceptionListener instead of overriding ErrorController

--- src/AppBundle/EventListeners/ExceptionListener.php
+++ src/AppBundle/EventListeners/ExceptionListener.php
@@ -10,43 +10,101 @@
 
 namespace AppBundle\EventListeners;
 
+use AppBundle\EventNotFoundHttpException;
 use AppBundle\InvalidTokenHttpException;
+use AppBundle\JsonResponse;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Debug\Exception\FlattenException;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Psr\Log\NullLogger;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Twig\Environment;
 
 class ExceptionListener
 {
     /**
      * @var LoggerInterface
      */
-    private $logger;
-
+    private LoggerInterface $logger;
+    
     /**
+     * @var Environment
+     */
+    private Environment $twig;
+    
+    /**
+     * @param Environment $twig
      * @param null|LoggerInterface $logger
      */
-    public function __construct(LoggerInterface $logger = null)
+    public function __construct(Environment $twig, ?LoggerInterface $logger = null)
     {
-        $this->logger = $logger;
+        $this->twig   = $twig;
+        $this->logger = $logger ?: new NullLogger();
     }
-
-	/**
-	 * @param GetResponseForExceptionEvent $event
-	 */
-	public function onKernelException(GetResponseForExceptionEvent $event) {
-		if ($this->logger === null) {
-			return;
-		}
-		$exception = $event->getException();
-		if ($exception instanceof InvalidTokenHttpException) {
-			return; //do not log token exceptions
-		}
-
-		$flattenException = FlattenException::create($exception);
-		$this->logger->error('Stack trace');
-		foreach ($flattenException->getTrace() as $trace) {
-			$traceMessage = sprintf('  at %s line %s', $trace['file'], $trace['line']);
-			$this->logger->error($traceMessage);
-		}
-	}
+    
+    /**
+     * @param ExceptionEvent $event
+     */
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $request       = $event->getRequest();
+        $accept        = $request->headers->get('accept', '');
+        $requestIsJson = strpos($accept, 'application/json') !== false
+                         || strpos($accept, 'text/javascript') !== false;
+        
+        $exception = $event->getThrowable();
+        $response  = null;
+        $doLog     = true;
+        if ($exception instanceof EventNotFoundHttpException) {
+            $doLog    = false;
+            $response = new Response($this->twig->render('event/public/miss.html.twig'), Response::HTTP_NOT_FOUND);
+        } elseif ($exception instanceof InvalidTokenHttpException) {
+            $doLog = false;
+            if ($requestIsJson) {
+                $response = JsonResponse::createError(
+                    'In der Zwischenzeit haben sich Teile dieser Seite geändert. Sie müssen sie aktualisieren, um fortfahren zu können.'
+                );
+            } else {
+                $response = new Response(
+                    $this->twig->render('event/public/token-outdated.html.twig'), Response::HTTP_BAD_REQUEST
+                );
+            }
+        } elseif ($exception instanceof AccessDeniedHttpException) {
+            if ($requestIsJson) {
+                $response = JsonResponse::createError(
+                    'Sie haben nicht die nötigen Berechtigung um die gewünschte Aktion durchzuführen.'
+                )->setStatusCode(Response::HTTP_FORBIDDEN);
+            } else {
+                $response = new Response(
+                    $this->twig->render('event/public/unallowed.html.twig'), Response::HTTP_FORBIDDEN
+                );
+            }
+        }
+        if ($response) {
+            $event->setResponse($response);
+        }
+        
+        if ($doLog) {
+            $this->logger->error(
+                'Exception {message} in {file} at {line}',
+                [
+                    'message' => $exception->getMessage(),
+                    'file'    => $exception->getFile(),
+                    'line'    => $exception->getLine()
+                ]
+            );
+            if ($exception instanceof \Exception) {
+                $this->logger->error('Stack trace');
+                $flattenException = FlattenException::create($exception);
+                foreach ($flattenException->getTrace() as $trace) {
+                    $traceMessage = sprintf('  at %s line %s', $trace['file'], $trace['line']);
+                    $this->logger->error($traceMessage);
+                }
+            } elseif ($exception instanceof \Throwable) {
+                $this->logger->error('Stack trace ' . $exception->getTraceAsString());
+            }
+        }
+        
+    }
 }
