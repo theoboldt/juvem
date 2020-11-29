48a781d64d612bb983351f308f94df12df4591fb
theoboldt <erik@theoboldt.eu>
2020-11-29 14:28:41 +0100

    Added additional sheet tests

--- app/tests/Export/Excel/CustomizedParticipantsExportTest.php
+++ app/tests/Export/Excel/CustomizedParticipantsExportTest.php
@@ -15,6 +15,7 @@ namespace Tests\Export\Excel;
 use AppBundle\BitMask\ParticipantFood;
 use AppBundle\Entity\Participant;
 use AppBundle\Export\Customized\CustomizedExport;
+use AppBundle\Manager\Payment\PaymentManager;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Tests\Export\TmpDirAccessingTestTrait;
@@ -24,14 +25,14 @@ class CustomizedParticipantsExportTest extends JuvemKernelTestCase
 {
     use ParticipantTestingDataTrait, TmpDirAccessingTestTrait;
 
-    public function testExport(): void
+    /**
+     * Provide participants
+     *
+     * @return Participant[]
+     */
+    private function provideParticipants(): array
     {
-        $kernel = static::bootKernel();
-        $kernel->boot();
-
-        $user  = $this->user();
-        $event = $this->event();
-
+        $event          = $this->event();
         $participation1 = $this->participation($event);
 
         $participant1 = new Participant();
@@ -59,6 +60,19 @@ class CustomizedParticipantsExportTest extends JuvemKernelTestCase
         $participant3->setFood(new ParticipantFood(2));
         $participant3->setGender(1);
         $participation1->addParticipant($participant3);
+
+        return [$participant1, $participant2, $participant3];
+    }
+
+    public function testDefaultExport(): void
+    {
+        $kernel = static::bootKernel();
+        $kernel->boot();
+
+        $user         = $this->user();
+        $participants = $this->provideParticipants();
+        $participant  = reset($participants);
+        $event        = $participant->getEvent();
 
         $configuration = [
             'filter'           =>
@@ -108,15 +122,14 @@ class CustomizedParticipantsExportTest extends JuvemKernelTestCase
         ];
         $export        = new CustomizedExport(
             $this->customization(),
-            $kernel->getContainer()->get(\AppBundle\Manager\Payment\PaymentManager::class),
+            $kernel->getContainer()->get(PaymentManager::class),
             $event,
-            [$participant1, $participant2, $participant3],
+            $participants,
             $user,
             $configuration
         );
         $export->setMetadata();
 
-        $export->setMetadata();
         $export->process();
 
         $tmpPath       = __DIR__ . '/../../../../var/tmp/' . uniqid('export_test');
@@ -125,6 +138,9 @@ class CustomizedParticipantsExportTest extends JuvemKernelTestCase
 
         $spreadsheet = IOFactory::load($tmpPath);
         $sheet       = $spreadsheet->getSheet(0);
+        
+        $this->assertEquals('Teilnehmer', $sheet->getTitle());
+
         $this->assertEqualsSheetValue($sheet, 1, 2, 'Testchild');
         $this->assertEqualsSheetValue($sheet, 2, 2, 'Doe');
         $this->assertEqualsSheetValue($sheet, 3, 2, '01.01.2000');
@@ -186,6 +202,189 @@ class CustomizedParticipantsExportTest extends JuvemKernelTestCase
         $this->assertEquals('Juvem', $properties->getCategory());
         $this->assertEquals($user->fullname(), $properties->getCreator());
         $this->assertEquals($user->fullname(), $properties->getLastModifiedBy());
+    }
+
+    public function testParticipationSheet(): void
+    {
+        $kernel = static::bootKernel();
+        $kernel->boot();
+
+        $user         = $this->user();
+        $participants = $this->provideParticipants();
+        $participant  = reset($participants);
+        $event        = $participant->getEvent();
+
+        $configuration = [
+            'filter'           =>
+                [
+                    'confirmed'         => 'all',
+                    'paid'              => 'all',
+                    'rejectedwithdrawn' => 'notrejectedwithdrawn',
+                ],
+            'participant'      =>
+                [
+                    'nameFirst'         => true,
+                    'nameLast'          => true,
+                    'birthday'          => true,
+                    'ageAtEvent'        => 'completed',
+                    'gender'            => true,
+                    'foodVegetarian'    => true,
+                    'foodLactoseFree'   => true,
+                    'foodLactoseNoPork' => true,
+                    'infoMedical'       => true,
+                    'infoGeneral'       => true,
+                    'basePrice'         => true,
+                    'price'             => true,
+                    'toPay'             => true,
+                    'acquisitionFields' => [],
+                    'grouping_sorting'  =>
+                        [
+                            'grouping' => ['field' => 'nameFirst',],
+                            'sorting'  => ['field' => 'aid',],
+                        ],
+                ],
+            'participation'    =>
+                [
+                    'salutation'        => true,
+                    'nameFirst'         => true,
+                    'nameLast'          => true,
+                    'email'             => true,
+                    'phoneNumber'       => 'comma',
+                    'addressStreet'     => true,
+                    'addressCity'       => true,
+                    'addressZip'        => true,
+                    'acquisitionFields' =>
+                        [
+                        ],
+                ],
+            'title'            => 'Teilnehmer',
+            'additional_sheet' => [
+                'participation' => true,
+            ],
+        ];
+        $export        = new CustomizedExport(
+            $this->customization(),
+            $kernel->getContainer()->get(PaymentManager::class),
+            $event,
+            $participants,
+            $user,
+            $configuration
+        );
+        $export->setMetadata();
+        $export->process();
+
+        $tmpPath       = __DIR__ . '/../../../../var/tmp/' . uniqid('export_test');
+        self::$files[] = $tmpPath;
+        $export->write($tmpPath);
+
+        $spreadsheet = IOFactory::load($tmpPath);
+        $sheet       = $spreadsheet->getSheet(1);
+        $this->assertEquals('Anmeldungen', $sheet->getTitle());
+        
+
+        $this->assertEqualsSheetValue($sheet, 1, 2, 'Ms.');
+        $this->assertEqualsSheetValue($sheet, 2, 2, 'Maria');
+        $this->assertEqualsSheetValue($sheet, 3, 2, 'Doe');
+        $this->assertEqualsSheetValue($sheet, 4, 2, 'Musterstrasse 25');
+        $this->assertEqualsSheetValue($sheet, 5, 2, 'Musterstadt');
+        $this->assertEqualsSheetValue($sheet, 6, 2, '70000');
+        $this->assertEqualsSheetValue($sheet, 7, 2, 'doe+example@example.com');
+        $this->assertEqualsSheetValue($sheet, 8, 2, '0163000000 (Mobile)');
+        
+        $this->assertEqualsSheetValue($sheet, 10, 2, '3');
+    }
+    
+    public function testSubventionSheet(): void
+    {
+        $kernel = static::bootKernel();
+        $kernel->boot();
+
+        $user         = $this->user();
+        $participants = $this->provideParticipants();
+        $participant  = reset($participants);
+        $event        = $participant->getEvent();
+
+        $configuration = [
+            'filter'           =>
+                [
+                    'confirmed'         => 'all',
+                    'paid'              => 'all',
+                    'rejectedwithdrawn' => 'notrejectedwithdrawn',
+                ],
+            'participant'      =>
+                [
+                    'nameFirst'         => true,
+                    'nameLast'          => true,
+                    'birthday'          => true,
+                    'ageAtEvent'        => 'completed',
+                    'gender'            => true,
+                    'foodVegetarian'    => true,
+                    'foodLactoseFree'   => true,
+                    'foodLactoseNoPork' => true,
+                    'infoMedical'       => true,
+                    'infoGeneral'       => true,
+                    'basePrice'         => true,
+                    'price'             => true,
+                    'toPay'             => true,
+                    'acquisitionFields' => [],
+                    'grouping_sorting'  =>
+                        [
+                            'grouping' => ['field' => 'nameFirst',],
+                            'sorting'  => ['field' => 'aid',],
+                        ],
+                ],
+            'participation'    =>
+                [
+                    'salutation'        => true,
+                    'nameFirst'         => true,
+                    'nameLast'          => true,
+                    'email'             => true,
+                    'phoneNumber'       => 'comma',
+                    'addressStreet'     => true,
+                    'addressCity'       => true,
+                    'addressZip'        => true,
+                    'acquisitionFields' =>
+                        [
+                        ],
+                ],
+            'title'            => 'Teilnehmer',
+            'additional_sheet' => [
+                'subvention_request' => true,
+            ],
+        ];
+        $export        = new CustomizedExport(
+            $this->customization(),
+            $kernel->getContainer()->get(PaymentManager::class),
+            $event,
+            $participants,
+            $user,
+            $configuration
+        );
+        $export->setMetadata();
+        $export->process();
+
+        $tmpPath       = __DIR__ . '/../../../../var/tmp/' . uniqid('export_test');
+        self::$files[] = $tmpPath;
+        $export->write($tmpPath);
+
+        $spreadsheet = IOFactory::load($tmpPath);
+        $sheet       = $spreadsheet->getSheet(1);
+        $this->assertEquals('Teilnehmer - Zuschussantrag', $sheet->getTitle());
+
+        $this->assertEqualsSheetValue($sheet, 1, 2, 'Testchild');
+        $this->assertEqualsSheetValue($sheet, 2, 2, 'Doe');
+        $this->assertEqualsSheetValue($sheet, 3, 2, '01.01.2000');
+        $this->assertEqualsSheetValue($sheet, 4, 2, 'Musterstrasse 25, 70000 Musterstadt');
+        
+        $this->assertEqualsSheetValue($sheet, 1, 3, 'Secondchild');
+        $this->assertEqualsSheetValue($sheet, 2, 3, 'Doe');
+        $this->assertEqualsSheetValue($sheet, 3, 3, '10.01.2000');
+        $this->assertEqualsSheetValue($sheet, 4, 3, 'Musterstrasse 25, 70000 Musterstadt');
+        
+        $this->assertEqualsSheetValue($sheet, 1, 4, 'Thirdchild');
+        $this->assertEqualsSheetValue($sheet, 2, 4, 'Doe');
+        $this->assertEqualsSheetValue($sheet, 3, 4, '15.01.2000');
+        $this->assertEqualsSheetValue($sheet, 4, 4, 'Musterstrasse 25, 70000 Musterstadt');
     }
 
     protected function assertEqualsSheetValue(Worksheet $sheet, int $columnIndex, int $row, string $expect): void
