d9b552e791f43c5bad5ce8e70d55574c74134fa0
Erik Theoboldt <erik@theoboldt.eu>
2021-02-23 07:33:35 +0100

    Now automatically configuring cloud username when user is authenticated at cloud via basic authentication test

--- app/src/AppBundle/Controller/ApiController.php
+++ app/src/AppBundle/Controller/ApiController.php
@@ -10,19 +10,73 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Manager\Filesharing\NextcloudManager;
+use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController
 {
+    use DoctrineAwareControllerTrait;
+    
+    private \Symfony\Component\Security\Core\Security $security;
+    
+    private LoggerInterface $logger;
+    
+    private ?NextcloudManager $cloudManager;
+    
+    /**
+     * ApiController constructor.
+     *
+     * @param ManagerRegistry $doctrine
+     * @param \Symfony\Component\Security\Core\Security $security
+     * @param NextcloudManager|null $nextcloudManager
+     * @param LoggerInterface|null $logger
+     */
+    public function __construct(
+        ManagerRegistry $doctrine,
+        \Symfony\Component\Security\Core\Security $security,
+        ?NextcloudManager $nextcloudManager = null,
+        LoggerInterface $logger = null
+    )
+    {
+        $this->doctrine     = $doctrine;
+        $this->security     = $security;
+        $this->cloudManager = $nextcloudManager;
+        $this->logger       = $logger ?: new NullLogger();
+    }
     
     /**
      * @Route("/api/cloud", name="api_cloud")
      * @Security("is_granted('cloud_access')")
      */
-    public function statusCloudAction()
+    public function connectCloudAction()
     {
+        $user = $this->security->getUser();
+        
+        if (!$this->cloudManager) {
+            $this->logger->warning(
+                'Requested cloud connection for user {username} but cloud is disabled',
+                ['username' => $user->getUsername()]
+            );
+            return new Response('', Response::HTTP_FORBIDDEN);
+        }
+        
+        if ($user instanceof User && $user->getCloudUsername() === null) {
+            $user->setCloudUsername($user->getEmail());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            $this->logger->notice(
+                'Configured cloud username {username} for user {id}',
+                ['username' => $user->getUsername(), 'id' => $user->getId()]
+            );
+        }
+        
         return new Response('', Response::HTTP_NO_CONTENT);
     }
 }
