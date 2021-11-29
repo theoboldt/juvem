6fa9a6911357a4218e6b07f24fd9032946d43568
theoboldt <erik@theoboldt.eu>
2021-11-29 20:15:40 +0100

    Updated migration

--- app/Migrations/Version20211129180000.php
+++ app/Migrations/Version20211129180000.php
@@ -23,7 +23,7 @@ final class Version20211129180000 extends AbstractMigration
             "CREATE TABLE feedback_questionnaire_fillout (uuid VARCHAR(128) NOT NULL, eid INT NOT NULL, feedback_questionnaire JSON DEFAULT NULL COMMENT '(DC2Type:json_array)', additions LONGTEXT NOT NULL, modified_at DATETIME DEFAULT NULL, created_at DATETIME NOT NULL, INDEX IDX_56B707554FBDA576 (eid), PRIMARY KEY(uuid)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB"
         );
         $this->addSql(
-            "ALTER TABLE event ADD feedback_questionnaire JSON DEFAULT NULL COMMENT '(DC2Type:json_array)', ADD is_feedback_questionnaire_sent TINYINT(1) DEFAULT '0' NOT NULL, ADD feedback_day_after_event_count SMALLINT UNSIGNED DEFAULT 0 NOT NULL"
+            "ALTER TABLE event ADD feedback_questionnaire JSON DEFAULT NULL COMMENT '(DC2Type:json_array)', ADD is_feedback_questionnaire_enabled TINYINT(1) DEFAULT '0' NOT NULL, ADD is_feedback_questionnaire_sent TINYINT(1) DEFAULT '0' NOT NULL, ADD feedback_day_after_event_count SMALLINT UNSIGNED DEFAULT 0 NOT NULL"
         );
         $this->addSql(
             "ALTER TABLE feedback_questionnaire_fillout ADD CONSTRAINT FK_56B707554FBDA576 FOREIGN KEY (eid) REFERENCES event (eid) ON DELETE CASCADE"
@@ -33,7 +33,7 @@ final class Version20211129180000 extends AbstractMigration
     public function down(Schema $schema): void
     {
         $this->addSql(
-            'ALTER TABLE event DROP feedback_questionnaire, DROP is_feedback_questionnaire_sent, DROP feedback_day_after_event_count'
+            'ALTER TABLE event DROP feedback_questionnaire, DROP is_feedback_questionnaire_enabled, DROP is_feedback_questionnaire_sent, DROP feedback_day_after_event_count'
         );
         $this->addSql('DROP TABLE feedback_questionnaire_fillout');
     }
